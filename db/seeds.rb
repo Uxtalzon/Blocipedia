98d5065f3dc80a4f72c626fd8805707b4b43509f
Phil <uxtalzon@yahoo.com>
2015-05-22 02:01:55 +0000

    Seeding users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+member = User.new(
+  name:     'Member User',
+  email:    'member@example.com',   password: 'helloworld'
+  )
+member.skip_confirmation!
+member.save!
